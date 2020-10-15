bdd9e4cf4971894971e02f7abe59c88b81fdaa9a
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2020-10-15 18:44:33 -0300

    chore: create toSnakeCaseF, toSnakeCaseP, toCamelCaseF and toCamelCaseP

--- fp.js
+++ fp.js
@@ -1,11 +1,27 @@
 const R = require("ramda"),
+  F = require("fluture"),
   snakeCaseKeys = require("snakecase-keys"),
   camelCaseKeys = require("camelcase-keys");
 
 const toSnakeCase = R.curry(snakeCaseKeys);
 const toCamelCase = R.curry(camelCaseKeys);
 
+const toSnakeCaseF = R.curry((p, options) =>
+  R.pipe(R.map(toSnakeCase(options)))(F.attemptP(promise))
+);
+
+const toCamelCaseF = R.curry((p, options) =>
+  R.pipe(R.map(toCamelCase(options)))(F.attemptP(promise))
+);
+
+const toSnakeCaseP = R.curry((p, options) => F.promise(toSnakeCaseF));
+const toCamelCaseP = R.curry((p, options) => F.promise(toCamelCaseF));
+
 module.exports = {
   toSnakeCase,
   toCamelCase,
+  toSnakeCaseF,
+  toCamelCaseF,
+  toSnakeCaseP,
+  toCamelCaseP
 };
